cdc6607cccf3b204412e0ba9a27eb7e22fbe012c
Sandra Lavalley <Sandra Lavalley@PC-de-SandraLav.home>
2011-09-05 22:02:24 +0200

    small clean of code.

--- src/com/charabia/SMSReceiver.java
+++ src/com/charabia/SMSReceiver.java
@@ -16,152 +16,20 @@
  package com.charabia;
 
 import android.content.BroadcastReceiver;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+
 import android.os.Bundle;
+
 import android.telephony.SmsMessage;
+
 import android.widget.Toast;
  
-import android.net.Uri;
- 
-import android.app.PendingIntent;
-import android.app.Notification;
-import android.app.NotificationManager;
-
-import android.util.Log;
 
 public class SMSReceiver extends BroadcastReceiver 
 {
-	public static final String NOTIF_TAG = "CHARABIA";
-	
-	public static int id = 0;
-	
 	private final String ACTION_RECEIVE_SMS  = "android.provider.Telephony.SMS_RECEIVED";
 
-    protected void showNotification(Context context, CharSequence from, CharSequence message) {
-		id += 1;
-
-		// look up the notification manager service
-		NotificationManager nm = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-		// The details of our fake message
-		//CharSequence from = "Joe";
-		//CharSequence message = "message id " + id;
-
-		//Intent intent = new Intent(Intent.ACTION_VIEW);
-		//intent.setClassName(context, SmsViewActivity.class.getName());
-		//intent.putExtra("ID", (short)id);
-		Intent intent = new Intent("com.confidentialsms.SmsViewActivity.VIEW");
-		
-        // The PendingIntent to launch our activity if the user selects this notification
-        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intent
-                /*new Intent(Intent.ACTION_VIEW, ConfidentialSmsActivity.class.getName())*/, 0);
-
-        // The ticker text, this uses a formatted string so our message could be localized
-        String tickerText = "hello";//getString(R.string.imcoming_message_ticker_text, message);
-
-        // construct the Notification object.
-        Notification notif = new Notification(android.R.drawable.ic_menu_help /*R.drawable.stat_sample*/, tickerText,
-                System.currentTimeMillis());
-
-        // Set the info for the views that show in the notification panel.
-        notif.setLatestEventInfo(context, from, message, contentIntent);
-
-		notif.flags |= Notification.FLAG_AUTO_CANCEL;
-
-        /*
-        // On tablets, the ticker shows the sender, the first line of the message,
-        // the photo of the person and the app icon.  For our sample, we just show
-        // the same icon twice.  If there is no sender, just pass an array of 1 Bitmap.
-        notif.tickerTitle = from;
-        notif.tickerSubtitle = message;
-        notif.tickerIcons = new Bitmap[2];
-        notif.tickerIcons[0] = getIconBitmap();;
-        notif.tickerIcons[1] = getIconBitmap();;
-        */
-
-        // after a 0ms delay, vibrate for 250ms, pause for 100 ms and
-        // then vibrate for 500ms.
-        //notif.vibrate = new long[] { 0, 250, 100, 500};
-
-        // Note that we use R.layout.incoming_message_panel as the ID for
-        // the notification.  It could be any integer you want, but we use
-        // the convention of using a resource id for a string related to
-        // the notification.  It will always be a unique number within your
-        // application.
-        nm.notify(id /*R.string.imcoming_message_ticker_text*/, notif);
-    }
-
-    protected void showNotification(Context context, SmsMessage message) {
-		id += 1;
-
-		String messageBody = message.getMessageBody();
-		String phoneNumber = message.getDisplayOriginatingAddress();
-
-		// look up the notification manager service
-		NotificationManager nm = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-		// The details of our fake message
-		//CharSequence from = "Joe";
-		//CharSequence message = "message id " + id;
-
-		//Intent intent = new Intent(Intent.ACTION_VIEW);
-		//intent.setClassName(context, SmsViewActivity.class.getName());
-		//intent.putExtra("ID", (short)id);
-		Intent intent = new Intent(SmsViewActivity.class.getName());//"com.charabia.SmsViewActivity.VIEW");
-		intent.putExtra("DATA", message.getPdu());
-		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		//intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-		//intent.addFlag(Intent.FLAG_ACTIVITY_NO_HISTORY);
-		//intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-		
-        // The PendingIntent to launch our activity if the user selects this notification
-        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intent, 0);
-
-        // The ticker text, this uses a formatted string so our message could be localized
-        String tickerText = "hello";//getString(R.string.imcoming_message_ticker_text, message);
-
-        // construct the Notification object.
-        Notification notif = new Notification(android.R.drawable.stat_notify_chat /*R.drawable.stat_sample*/, tickerText,
-                System.currentTimeMillis());
-
-        // Set the info for the views that show in the notification panel.
-        notif.setLatestEventInfo(context, phoneNumber, messageBody, contentIntent);
-
-		notif.flags |= Notification.FLAG_AUTO_CANCEL;
-		notif.flags |= Notification.FLAG_NO_CLEAR;
-		
-        /*
-        // On tablets, the ticker shows the sender, the first line of the message,
-        // the photo of the person and the app icon.  For our sample, we just show
-        // the same icon twice.  If there is no sender, just pass an array of 1 Bitmap.
-        notif.tickerTitle = from;
-        notif.tickerSubtitle = message;
-        notif.tickerIcons = new Bitmap[2];
-        notif.tickerIcons[0] = getIconBitmap();;
-        notif.tickerIcons[1] = getIconBitmap();;
-        */
-
-        // after a 0ms delay, vibrate for 250ms, pause for 100 ms and
-        // then vibrate for 500ms.
-        //notif.vibrate = new long[] { 0, 250, 100, 500};
-
-        // Note that we use R.layout.incoming_message_panel as the ID for
-        // the notification.  It could be any integer you want, but we use
-        // the convention of using a resource id for a string related to
-        // the notification.  It will always be a unique number within your
-        // application.
-        nm.notify(id /*R.string.imcoming_message_ticker_text*/, notif);
-    }
-
-	/*
-    private Bitmap getIconBitmap() {
-        BitmapFactory f = new BitmapFactory();
-        return f.decodeResource(getResources(), R.drawable.app_sample_code);
-    }
-	*/
-	
 	@Override
 	public void onReceive(Context context, Intent intent)
 	{
--- src/com/charabia/Tools.java
+++ src/com/charabia/Tools.java
@@ -155,101 +155,9 @@ public class Tools
 	{
 		putSmsToDatabase(contentResolver, sms.getDisplayOriginatingAddress(), 
 			System.currentTimeMillis(), MESSAGE_TYPE_INBOX, sms.getStatus(), sms.getMessageBody());
-		
-		
-		// Create SMS row
-		//ContentValues values = new ContentValues();
-		//values.put( ADDRESS, sms.getOriginatingAddress() );
-		//values.put( DATE, sms.getTimestampMillis() );
-		//values.put( READ, MESSAGE_IS_READ );
-		//values.put( STATUS, sms.getStatus() );
-		//values.put( TYPE, MESSAGE_TYPE_INBOX );
-		//values.put( SEEN, MESSAGE_IS_SEEN );
-		//values.put( BODY, sms.getMessageBody().toString() );
-		// Push row into the SMS table
-		//contentResolver.insert( Uri.parse( SMS_URI ), values );
 	}
 
 	static public void showNotification(Context context, SmsMessage message) {
-		//id += 1;
-		
-		SmsViewActivity.smsListe.add(message);
-
-		int nbMessages = SmsViewActivity.smsListe.size();
-
-		String messageBody = message.getMessageBody();
-		String phoneNumber = message.getDisplayOriginatingAddress();
-
-		// look up the notification manager service
-		NotificationManager nm = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-		// The details of our fake message
-		//CharSequence from = "Joe";
-		//CharSequence message = "message id " + id;
-
-		//Intent intent = new Intent(Intent.ACTION_VIEW);
-		//intent.setClassName(context, SmsViewActivity.class.getName());
-		//intent.putExtra("ID", (short)id);
-		Intent intent = new Intent(SmsViewActivity.class.getName());//"com.charabia.SmsViewActivity.VIEW");
-		intent.putExtra("DATA", message.getPdu());
-		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		//intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-		//intent.addFlag(Intent.FLAG_ACTIVITY_NO_HISTORY);
-		//intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-		
-        // The PendingIntent to launch our activity if the user selects this notification
-        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intent, 0);
-
-        // The ticker text, this uses a formatted string so our message could be localized
-        String tickerText = context.getString(R.string.imcoming_message_ticker_text, nbMessages);
-
-        // construct the Notification object.
-        Notification notif = new Notification(android.R.drawable.stat_notify_chat /*R.drawable.stat_sample*/, tickerText,
-                System.currentTimeMillis());
-
-        // Set the info for the views that show in the notification panel.
-        notif.setLatestEventInfo(context, phoneNumber, messageBody, contentIntent);
-
-		//notif.flags |= Notification.FLAG_AUTO_CANCEL;
-		notif.flags |= Notification.FLAG_NO_CLEAR;
-		
-        /*
-        // On tablets, the ticker shows the sender, the first line of the message,
-        // the photo of the person and the app icon.  For our sample, we just show
-        // the same icon twice.  If there is no sender, just pass an array of 1 Bitmap.
-        notif.tickerTitle = from;
-        notif.tickerSubtitle = message;
-        notif.tickerIcons = new Bitmap[2];
-        notif.tickerIcons[0] = getIconBitmap();;
-        notif.tickerIcons[1] = getIconBitmap();;
-        */
-
-        // after a 0ms delay, vibrate for 250ms, pause for 100 ms and
-        // then vibrate for 500ms.
-        //notif.vibrate = new long[] { 0, 250, 100, 500};
-
-        // Note that we use R.layout.incoming_message_panel as the ID for
-        // the notification.  It could be any integer you want, but we use
-        // the convention of using a resource id for a string related to
-        // the notification.  It will always be a unique number within your
-        // application.
-        nm.notify(R.string.imcoming_message_ticker_text, notif);
-    }
-
-
-	static public void showNotification(Context context) {
-	
-		// look up the notification manager service
-		NotificationManager nm = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-		int nbMessages = SmsViewActivity.smsListe.size();
-
-		if(nbMessages < 0) {
-			nm.cancel(R.string.imcoming_message_ticker_text);
-		}
-		else {
-			SmsMessage message = SmsViewActivity.smsListe.lastElement();
-
 			String messageBody = message.getMessageBody();
 			String phoneNumber = message.getDisplayOriginatingAddress();
 
@@ -292,58 +200,25 @@ public class Tools
 			// the notification.  It will always be a unique number within your
 			// application.
 			nm.notify(R.string.imcoming_message_ticker_text, notif);
+    }
+
+
+	static public void showNotification(Context context) {
+	
+		// look up the notification manager service
+		NotificationManager nm = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+		int nbMessages = SmsViewActivity.smsListe.size();
+
+		if(nbMessages < 0) {
+			nm.cancel(R.string.imcoming_message_ticker_text);
+		}
+		else {
+			SmsMessage message = SmsViewActivity.smsListe.lastElement();
+			showNotification(context, message);
 		}
     }
 
-	static public void showNotificationTest(Context context) {
-	
-	
-			// look up the notification manager service
-			NotificationManager nm = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-			int nbMessages = -1;
-			
-			Intent intent = new Intent(SmsViewActivity.class.getName());
-			intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-			// The PendingIntent to launch our activity if the user selects this notification
-			PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intent, 0);
-
-			// The ticker text, this uses a formatted string so our message could be localized
-			String tickerText = context.getString(R.string.imcoming_message_ticker_text, nbMessages);
-
-			// construct the Notification object.
-			Notification notif = new Notification(android.R.drawable.stat_notify_chat, tickerText,
-				System.currentTimeMillis());
-
-			// Set the info for the views that show in the notification panel.
-			notif.setLatestEventInfo(context, "test", "test", contentIntent);
-
-			notif.flags |= Notification.FLAG_NO_CLEAR;
-			/*
-			// On tablets, the ticker shows the sender, the first line of the message,
-			// the photo of the person and the app icon.  For our sample, we just show
-			// the same icon twice.  If there is no sender, just pass an array of 1 Bitmap.
-			notif.tickerTitle = from;
-			notif.tickerSubtitle = message;
-			notif.tickerIcons = new Bitmap[2];
-			notif.tickerIcons[0] = getIconBitmap();;
-			notif.tickerIcons[1] = getIconBitmap();;
-			*/
-
-			// after a 0ms delay, vibrate for 250ms, pause for 100 ms and
-			// then vibrate for 500ms.
-			//notif.vibrate = new long[] { 0, 250, 100, 500};
-
-			// Note that we use R.layout.incoming_message_panel as the ID for
-			// the notification.  It could be any integer you want, but we use
-			// the convention of using a resource id for a string related to
-			// the notification.  It will always be a unique number within your
-			// application.
-			nm.notify(R.string.imcoming_message_ticker_text, notif);
-		
-    }
-	
 	public static SecretKey generateKeyAES(Context context) {
 		try {
 			KeyGenerator keyGen = KeyGenerator.getInstance("AES");
